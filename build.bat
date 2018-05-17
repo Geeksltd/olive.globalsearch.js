61a1c4f506649432f39783299726f27d079d0e09
Mohsen <mohsens2@outlook.com>
2018-05-17 17:16:12 +0430

    no message

--- build.bat
+++ build.bat
@@ -1,12 +1,4 @@
 ﻿@echo off
-
 ECHO Use Visual Studio Code to edit this project.
-
-ECHO Checking tsc node module is installed globally...
-
-where tsc > nul
-if ERRORLEVEL 1 (	
-	npm install typescript -g
-)
-
-tsc
+call bower install
+call tsc
